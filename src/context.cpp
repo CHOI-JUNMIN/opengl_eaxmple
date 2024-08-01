5141f2b7104288971b2459b9e3b476ba78f68cfa
최준민 <csm0791@naver.com>
2024-08-01 16:35:20 +0900

    20240801 container

--- src/context.cpp
+++ src/context.cpp
@@ -1,4 +1,5 @@
 #include "context.h"
+#include "image.h"
 
 ContextUPtr Context::Create()
 {
@@ -11,10 +12,38 @@ ContextUPtr Context::Create()
 bool Context::Init()
 {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right, red
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right, green
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  // top left, yellow
+        0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        1.0f,
+        0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        -0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        -0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
     };
     uint32_t indices[] = {
         // note that we start from 0!
@@ -23,15 +52,16 @@ bool Context::Init()
     };
 
     m_vertexLayout = VertexLayout::Create();
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 32);
 
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);                 // 하나의 점전 크기 6 , 처음부터니까 offset 0
-    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3); // 새로운attrib(color) 3개를 뛰어넘어야 color가 나오므로 sizeof(float)*3
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3);
+    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -41,11 +71,24 @@ bool Context::Init()
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
 
-    /*  auto loc = glGetUniformLocation(m_program->Get(), "color"); // simple.fs에 있는 color를 가져옴
-      m_program->Use();
-      glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f); // fluniform함수가 많음  4f 플로팅포인트 값이 4개 v가 붙으면 포인터
+    glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
 
-      glClearColor(0.0f, 0.1f, 0.2f, 0.0f);*/
+    auto image = Image::Load("./image/container.jpg");
+    if (!image)
+        return false;
+    SPDLOG_INFO("image: {}x{}, {} channels",
+                image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);    // MIN: 이미지가 많이 축소되었을때
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);    // MAX: 이미지가 많이 확대되었을때
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // 텍스쳐 좌표계의 X  0< or 1>일때 제일 모서리값을 씀
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // 텍스쳐 좌표계의 Y  0< or 1>일때 제일 모서리값을 씀
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,                    // 0: 레벨값
+                 image->GetWidth(), image->GetHeight(), 0,    // 0: 외각선(?) border
+                 GL_RGB, GL_UNSIGNED_BYTE, image->GetData()); // 여기 RGB: 이미지에 픽셀type
 
     return true;
 }
