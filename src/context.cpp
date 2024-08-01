9636c40a3bbe7076b08b6d24d49c493bc71e33c3
최준민 <csm0791@naver.com>
2024-08-01 14:20:57 +0900

    20240801 GLSL

--- src/context.cpp
+++ src/context.cpp
@@ -11,26 +11,27 @@ ContextUPtr Context::Create()
 bool Context::Init()
 {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f,   // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f,  // top left
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  // top left, yellow
     };
     uint32_t indices[] = {
         // note that we start from 0!
         0, 1, 3, // first triangle
         1, 2, 3, // second triangle
     };
+
     m_vertexLayout = VertexLayout::Create();
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
 
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 12);
-
-    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);                 // 하나의 점전 크기 6 , 처음부터니까 offset 0
+    m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3); // 새로운attrib(color) 3개를 뛰어넘어야 color가 나오므로 sizeof(float)*3
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
-    ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
     if (!vertShader || !fragShader)
         return false;
     SPDLOG_INFO("vertex shader id: {}", vertShader->Get());
@@ -40,7 +41,11 @@ bool Context::Init()
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
 
-    glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+    /*  auto loc = glGetUniformLocation(m_program->Get(), "color"); // simple.fs에 있는 color를 가져옴
+      m_program->Use();
+      glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f); // fluniform함수가 많음  4f 플로팅포인트 값이 4개 v가 붙으면 포인터
+
+      glClearColor(0.0f, 0.1f, 0.2f, 0.0f);*/
 
     return true;
 }
